ada7314177e9eedcebce7b9e7f23a01615f28e8d
shakuvi <shakuviakash201@gmail.com>
2023-05-01 20:34:47 +0530

    Food table created with pegination

---
+++ src/components/food/FoodCatergorySelector.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FoodCatergorySelector() {
+  return <div>FoodCatergorySelector</div>;
+}
--- src/pages/CatergoryFoods.js
+++ src/pages/CatergoryFoods.js
@@ -1,7 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CommonLayout from "../layouts/common/CommonLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllFoodsByCatergory } from "../store/actions/foodAction";
+import {
+  Grid,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TablePagination,
+} from "@mui/material";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteSweepOutlinedIcon from "@mui/icons-material/DeleteSweepOutlined";
 
 export default function CatergoryFoods() {
   const dispatch = useDispatch();
@@ -9,15 +21,84 @@ export default function CatergoryFoods() {
     (store) => store.catergoryReducer
   );
 
+  const { allFoodList } = useSelector((store) => store.foodReducer);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
   useEffect(() => {
     dispatch(getAllFoodsByCatergory(userSelectedCatergory._id));
   }, [dispatch, userSelectedCatergory]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   console.log(userSelectedCatergory);
 
   return (
     <div>
-      <CommonLayout></CommonLayout>
+      <CommonLayout>
+        <div style={{ paddingLeft: 10, paddingRight: 10 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontWeight: "bold" }}>Name</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>Price</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>
+                  Description
+                </TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>Image</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? allFoodList.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : allFoodList
+              ).map((val, key) => (
+                <TableRow key={key}>
+                  <TableCell>{val.name}</TableCell>
+                  <TableCell>{val.price}</TableCell>
+                  <TableCell>{val.description}</TableCell>
+                  <TableCell>{val.image}</TableCell>
+                  <TableCell>
+                    <Grid container justifyContent="space-around">
+                      <Grid item>
+                        <IconButton color="secondary">
+                          <EditOutlinedIcon />
+                        </IconButton>
+                      </Grid>
+                      <Grid item>
+                        <IconButton color="primary">
+                          <DeleteSweepOutlinedIcon />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={allFoodList.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </div>
+      </CommonLayout>
     </div>
   );
 }
