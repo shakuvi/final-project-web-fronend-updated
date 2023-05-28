9eb681fc1b3b189ef89c873102027dd209af676d
shakuvi <shakuviakash201@gmail.com>
2023-05-28 18:35:27 +0530

    Errors fixed

--- src/store/actions/employeeAction.js
+++ src/store/actions/employeeAction.js
@@ -98,7 +98,7 @@ export const createEmployee = (employee, token) => {
     dispatch({ type: CREATE_EMPLOYEE_START });
     axios
       .post(
-        "http://localhost:5000/employee/create",
+        "https://plymouthfinal.live:5000/employee/create",
         {
           employee,
         },
@@ -132,7 +132,7 @@ export const employeeLogin = (email, password) => {
   return (dispatch) => {
     dispatch({ type: EMPOLYEE_LOGIN_START });
     axios
-      .post("http://localhost:5000/employee/sign-in", {
+      .post("https://plymouthfinal.live:5000/employee/sign-in", {
         email,
         password,
       })
